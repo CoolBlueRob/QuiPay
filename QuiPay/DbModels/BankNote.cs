23b3e77bb7a6e73cc4657710f2dfa9c234164f9b
RobCoolBlue <rob@coolbluesoftware.com>
2020-05-23 05:38:24 +0100

    Added BankNote data

---
+++ QuiPay/DbModels/BankNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiPay.DbModels
+{
+    public enum BankNoteState
+    {
+        Unprinted, Printed, Suspended
+    }
+
+    public class BankNote
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public BankNoteState BankNoteState { get; set; }
+
+        public int Printed { get; set; }
+
+        public Currency Value { get; set; }
+    }
+}
